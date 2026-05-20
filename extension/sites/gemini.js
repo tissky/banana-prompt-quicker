d5fcf221ae15e4d49b33f97ec993e59a4b59366d
glidea <740696441@qq.com>
2026-05-20 17:43:28 +0800

    fix: adapt Gemini image tool selector

--- extension/sites/gemini.js
+++ extension/sites/gemini.js
@@ -4,7 +4,7 @@ class GeminiSite extends BaseSite {
     }
 
     async findTargetButton() {
-        return this.findElement('gemini', 'insertButton', 'button.toolbox-drawer-item-deselect-button:has(mat-icon[fonticon="photo_prints"])');
+        return this.findElement('gemini', 'insertButton', 'toolbox-drawer [data-test-id="deselect-drawer-item-gem-button"]');
     }
 
     getCurrentTheme() {
